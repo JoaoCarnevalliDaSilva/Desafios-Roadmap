30d4e03289065accf7ea3648f63bd460867ce6b6
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-08-03 21:35:14 -0300

    atualiza

--- 3 - JavaScript/1 - script.js
+++ 3 - JavaScript/1 - script.js
@@ -1,2 +1,11 @@
-var nome = 'Joao';
-console.log(nome);
+var hora = new Date().getHours()
+    var saudacao
+    if(hora < 12){
+      saudacao = "Bom dia"
+    } else if(hora < 18){
+      saudacao = "Boa tarde"
+    } else {
+      saudacao = "Boa noite"
+    }
+
+    document.getElementById("mensagem").innerHTML = saudacao;
