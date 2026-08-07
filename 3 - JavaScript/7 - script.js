2eb8872005e61b8ba07ca062aa780153b32291fc
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-08-06 22:16:14 -0300

    atualiza

--- 3 - JavaScript/7 - script.js
+++ 3 - JavaScript/7 - script.js
@@ -3,13 +3,14 @@ function calcular(event) {
   const salario = parseFloat(document.querySelector("#salario").value);
   const nota = parseFloat(document.querySelector("#nota").value);
   const resultado = document.querySelector("#resultado");
-  if (nota = 5) {
+  let valorBonus = 0;
+  if (nota === 5) {
     valorBonus = salario * 2;
     resultado.innerHTML = `Valor do bonus é de R$ ${valorBonus} e o montante total a receber é de R$ ${salario+valorBonus}`;
-  } else if(nota = 4) {
+  } else if(nota === 4) {
     valorBonus = salario;
     resultado.innerHTML = `Valor do bonus é de R$ ${valorBonus} e o montante total a receber é de R$ ${salario+valorBonus}`;
-  } else if (nota = 3){
+  } else if (nota === 3){
     valorBonus = salario * 0.5; 
     resultado.innerHTML = `Valor do bonus é de R$ ${valorBonus} e o montante total a receber é de R$ ${salario+valorBonus}`; 
   } else {
